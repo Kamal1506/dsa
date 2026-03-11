fba2e5f41ae7591a06dd2218adce2026ade8b328
Kamal Arumugam <kamalak1506@gmail.com>
2026-03-11 20:42:06 +0530

    neetcode problem

---
+++ appendCharacter.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int appendCharacters(String s, String t) {
+        int i = 0;
+        int j = 0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        return t.length() - j;
+    }
+}
+
+// practice at : https://neetcode.io/problems/append-characters-to-string-to-make-subsequence/question
