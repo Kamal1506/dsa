117279c1bff0797bb3e65dabb4a7b84d96007fdb
Kamal Arumugam <kamalak1506@gmail.com>
2026-07-16 21:32:47 +0530

    leetcode 2723

---
+++ add2Promises.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const [value1, value2] = await Promise.all([promise1, promise2]);
+    return value1+value2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
+
+// await gives the promises resolved value
+// we store that values in value1 and value2 and return by adding those
+
+// https://leetcode.com/problems/add-two-promises/
