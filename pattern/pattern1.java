313313ae2d9e48eeab15f63783c0dbd0e0ebf6a4
Kamal Arumugam <kamalak1506@gmail.com>
2026-08-01 11:48:51 +0530

    Add pattern1.java to print a star pattern
    Implement a pattern printing program in Java.

---
+++ pattern/pattern1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int n = 5;
+      for(int i = 1; i<2*n; i++){
+        int column = i>n ? (2*n)-i : i;
+        for(int j = 1; j<=column; j++){
+          System.out.print("* ");
+        }
+        System.out.println();
+      }
+    }
+}
+
+
+print:
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
