b8646b8bef1bc8b0643295ae13fdee494f8dd799
Kamal Arumugam <kamalak1506@gmail.com>
2026-08-03 16:32:19 +0530

    Kadane's Algorithm

---
+++ maximumSubArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for(int num : nums){
+            currSum = Math.max(num, currSum + num);
+            maxSum = Math.max(currSum, maxSum);
+        }
+        return maxSum;
+    }
+}
+
+// https://leetcode.com/problems/maximum-subarray/
